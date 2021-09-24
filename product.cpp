fc55e5d98d814a6612a2f02cce57e71cee940cba
Shreya2417 <87252188+Shreya2417@users.noreply.github.com>
2021-09-24 11:36:54 +0530

    Create product.cpp

---
+++ product.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float a,b,product;
+    cout<< "Enter a Number";
+    cin>>a;
+    cout<<"Enter Another Number";
+    cin>>b;
+    product= a*b;
+    cout<<"product="<<product;
+    return 0;
+}
